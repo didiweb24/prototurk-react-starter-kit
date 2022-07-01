8016557e0fd5728cd8ec7945a057978f13829673
didiweb24 <oktaydilan1@gmail.com>
2022-07-02 01:02:26 +0300

    env updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 function App() {
   return (
     <div className="App">
+      <h3>{process.env.NODE_ENV}</h3>
       <img src="./logo192.png" />
       <img src={logo} />
     </div>
